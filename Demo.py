56411efa928b4f5efd75573e8a56d2d6c839bd3a
LAPTOP-2K7L5JQA\kaich <zhiqian.zhao@oppomobile.nz>
2022-12-19 16:10:08 +1300

    change demo and mian script

--- Demo.py
+++ Demo.py
@@ -1,19 +1,33 @@
-# from ppadb.client import Client as AdbClient
-# import utils.PhoneUtils
-import utils.CommonUtils
+from ppadb.client import Client as AdbClient
 
-utils.CommonUtils.project_initial()
+import utils.AdbUtils
+from utils import Logger
 
-# Default is "127.0.0.1" and 5037
-# client = AdbClient(host="127.0.0.1", port=5037)
-# print(client.version())
-# device = client.devices()[0]
-# utils.PhoneUtils.take_snapshot(device)
-logger = utils.CommonUtils.common_logger()
-# logger = utils.CommonUtils.logger()
 
-logger.debug("11111")
-logger.info("11111")
-logger.warning("11111")
-logger.error("11111")
-logger.critical("11111")
+class Demo:
+    def __init__(self):
+        pass
+
+    def testLogger(self):
+        # logger = utils.CommonUtils.common_logger()
+        Logger.debug("11111")
+        Logger.info("11111")
+        Logger.warning("11111")
+        Logger.error("11111")
+        Logger.critical("11111")
+
+    def test_adb(self):
+        # Default is "127.0.0.1" and 5037
+
+        client = AdbClient(host="127.0.0.1", port=5037)
+        print(client.version())
+        device = client.devices()[0]
+        # utils.PhoneUtils.take_snapshot(device)
+        utils.AdbUtils.take_snapshot(device)
+
+
+# test_adb()
+if __name__ == '__main__':
+    d = Demo()
+    # d.testLogger()
+    d.test_adb()
